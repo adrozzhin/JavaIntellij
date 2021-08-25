0ecde6657d0af2afe31dab109d8f96e284124a31
adrozzhin <drozzhinandrei@gmail.com>
2021-08-25 16:35:55 -0700

    Class 17 HWv2

---
+++ src/com/syntax/class17HW/Task2_2.java
@@ -0,0 +1,7 @@
+package com.syntax.class17HW;
+
+public class Task2_2 {
+    public  String reverse(String inputStr) {
+        return new StringBuilder(inputStr).reverse().toString();
+    }
+}
---
+++ src/com/syntax/class17HW/Task2_2Tester.java
@@ -0,0 +1,7 @@
+package com.syntax.class17HW;
+
+public class Task2_2Tester {
+    public static void main(String[] args) {
+        System.out.println(new Task2_2().reverse("Sunday"));
+    }
+}
